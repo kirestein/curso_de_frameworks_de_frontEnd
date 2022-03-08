1ea25ba7ffa7ee33c3633482adccc39bcbc3e897
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-08 19:35:47 -0300

    created the project express  in node js

---
+++ projetos-express/src/index.js
@@ -0,0 +1,31 @@
+// primeiro app node-express
+// variável que receberá o módulo express
+
+var express = require('express')
+
+//objeto para fazermos uso dentro da aplicação
+var app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World NodeJS! Componente padrão')
+})
+
+app.get('/uma-lista', (req, res) => {
+    res.send('Esse é o conteúdo para uma página componente (uma lista)')
+})
+
+app.get('/zf*zy', (req, res) => {
+    res.send('Novo componente acessado a partir de uma rota customizada')
+})
+
+//vamos implementar o escutador da requisição
+var server = app.listen(8081, () => {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log(`O servidor pode ser acessado em http://${host}:${port}`)
+    // console.log('O servidor pode ser acessado em http://%s:%s', host, port)
+})
+
+
+
