7ff0bbfe40d6035544c8827736fb40975f57fae6
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-21 20:46:09 -0300

    Atualizando o estudo de var

--- aprendendoTypescript/usandoVar.js
+++ aprendendoTypescript/usandoVar.js
@@ -1,4 +1,3 @@
-var nome = 'Erik';
 var p1 = 95;
 var p2 = 38;
 var total = p1 + p2;
@@ -9,7 +8,7 @@ function calculandoAMedia() {
         console.log("".concat(nome, ", parab\u00E9ns! Voc\u00EA foi aprovado! Sua m\u00E9dia foi ").concat(media, ", seu status de formado \u00E9 ").concat(formado));
         console.log('Você está formado');
     }
-    else if (media >= 3 && media <= 6) {
+    else if (media >= 30 && media < 70) {
         console.log("".concat(nome, ", voc\u00EA est\u00E1 de recupera\u00E7\u00E3o! Sua m\u00E9dia foi ").concat(media));
     }
     else {
@@ -22,4 +21,24 @@ function calculandoAMedia() {
     console.log(media);
     // console.log(formado)
 }
+var nome = 'Erik';
 calculandoAMedia();
+////////////////////////////////////////////////////////////////////////////////////////////////
+var num1 = 1;
+function declarandoVar() {
+    var num2 = 2;
+    var num3 = 3;
+    if (num1 < num2) {
+        num3++;
+    }
+    var num4 = 4;
+    while (num1 < num2) {
+        num4++;
+        num1++;
+    }
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+    console.log(num4);
+}
+declarandoVar();
--- aprendendoTypescript/usandoVar.ts
+++ aprendendoTypescript/usandoVar.ts
@@ -1,16 +1,15 @@
-var nome:string = 'Erik'
-var p1:number = 95
-var p2:number = 38
-var total:number = p1 + p2
+var p1: number = 95
+var p2: number = 38
+var total: number = p1 + p2
 
 function calculandoAMedia() {
-  var media:number = total / 2
+  var media: number = total / 2
 
-  if(media >= 70) {
-    var formado:boolean = true
+  if (media >= 70) {
+    var formado: boolean = true
     console.log(`${nome}, parabéns! Você foi aprovado! Sua média foi ${media}, seu status de formado é ${formado}`)
     console.log('Você está formado')
-  } else if (media >=3 && media <= 6) {
+  } else if (media >= 30 && media < 70) {
     console.log(`${nome}, você está de recuperação! Sua média foi ${media}`)
   } else {
     console.log(`${nome}, você foi reprovado! Sua média foi ${media}`)
@@ -21,7 +20,32 @@ function calculandoAMedia() {
   console.log(total)
   console.log(media)
   // console.log(formado)
-  
+
 }
+var nome: string = 'Erik'
 
 calculandoAMedia()
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+var num1: number = 1
+
+function declarandoVar() {
+  var num2: number = 2
+  var num3: number = 3
+
+  if (num1 < num2) {
+    num3++
+  }
+  var num4: number = 4
+
+  while(num1 < num2){
+    num4++
+    num1++
+  }
+  console.log(num1)
+  console.log(num2)
+  console.log(num3)
+  console.log(num4)
+}
+declarandoVar()
