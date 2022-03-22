f3861ad4f919b965e37b905d2375086243d14914
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-21 21:53:02 -0300

    Estudando const

--- aprendendoTypescript/usandoConst.js
+++ aprendendoTypescript/usandoConst.js
@@ -1,13 +1,40 @@
-var numConst = 100;
+/* const numConst: number = 100
+
 // numConst = Compiler Error: Cannot assign to 'numCons' because it is a constant
-var participantes = {
-    participante1: 'Erik',
-    participante2: 'Pedro',
-    participante3: 'Marcelo',
-    participante4: 5,
-    participante5: true
+
+const participantes = {
+  participante1: 'Erik',
+  participante2: 'Pedro',
+  participante3: 'Marcelo',
+  participante4: 5,
+  participante5: true
+}
+
+console.log(participantes)
+
+participantes.participante5 = false
+
+console.log(participantes)
+
+console.log(participantes.participante3) */
+var vidaGatos = 9;
+//criando um objeto literal
+var dadosGatos = {
+    nome: 'Astolfo',
+    QtdeDeVidas: vidaGatos,
+    filhotes: {
+        filhote1: 'Piu Piu',
+        filhote2: 'Fofinho',
+        filhote3: 'Zézinho'
+    }
 };
-console.log(participantes);
-participantes.participante5 = false;
-console.log(participantes);
-console.log(participantes.participante3);
+//exibir o objeto
+console.log(dadosGatos);
+//aplicar modificações nas propriedades do objeto
+dadosGatos.nome = 'Frajola';
+dadosGatos.QtdeDeVidas = 96;
+dadosGatos.filhotes.filhote1 = 'Huguinho';
+//exibir as alteraçõs nas propriedades do objeto
+console.log("A quantidade de vidas do gato ".concat(dadosGatos.nome, " \u00E9 de ").concat(dadosGatos.QtdeDeVidas));
+//exibir o objeto
+console.log(dadosGatos);
--- aprendendoTypescript/usandoConst.ts
+++ aprendendoTypescript/usandoConst.ts
@@ -1,4 +1,4 @@
-const numConst: number = 100
+/* const numConst: number = 100
 
 // numConst = Compiler Error: Cannot assign to 'numCons' because it is a constant
 
@@ -16,4 +16,32 @@ participantes.participante5 = false
 
 console.log(participantes)
 
-console.log(participantes.participante3)
+console.log(participantes.participante3) */
+
+let vidaGatos: number = 9
+
+//criando um objeto literal
+const dadosGatos = {
+  nome: 'Astolfo',
+  QtdeDeVidas: vidaGatos,
+  filhotes: {
+    filhote1: 'Piu Piu',
+    filhote2: 'Fofinho',
+    filhote3: 'Zézinho'
+  }
+}
+
+//exibir o objeto
+console.log(dadosGatos)
+
+//aplicar modificações nas propriedades do objeto
+dadosGatos.nome = 'Frajola'
+dadosGatos.QtdeDeVidas = 96
+dadosGatos.filhotes.filhote1 = 'Huguinho'
+
+//exibir as alteraçõs nas propriedades do objeto
+console.log(`A quantidade de vidas do gato ${dadosGatos.nome} é de ${dadosGatos.QtdeDeVidas}`)
+
+//exibir o objeto
+console.log(dadosGatos)
+
