0b4a1b289bffd768ab13bc0ee3050d649236d676
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-22 19:49:33 -0300

    Estudando laço do..while

---
+++ aprendendoTypescript/loopDoWhile.ts
@@ -0,0 +1,9 @@
+var novoNum: number = 13
+
+console.log('Valores encontrados durante a interação do loop do..while')
+
+//criando o loop
+do {
+  console.log(novoNum)
+  novoNum--
+} while (novoNum >= 1)
