1cbf204508678750d22522b3ee8a2d9f50b1a086
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-22 20:20:20 -0300

    Estudando Classes no TypeScript

---
+++ aprendendoTypescript/ClassesTypeScript/minhaPrimeiraClasse.ts
@@ -0,0 +1,7 @@
+// vamos criar nossa primeira classe em TypeScript
+class Saudacao {
+  //escopo da classe ou class scope
+  saudacao():void{
+    console.log('Olá mundo a partir do POO TS')
+  }
+}
