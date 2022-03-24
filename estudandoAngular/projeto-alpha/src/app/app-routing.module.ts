22849d9c9384661752d84111bc3f1f76fabe3953
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-24 20:17:24 -0300

    Adicionando o two way binding nas rotas

--- estudandoAngular/projeto-alpha/src/app/app-routing.module.ts
+++ estudandoAngular/projeto-alpha/src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 //importando os nossos componentes
 import { HomeComponent } from './home/home.component';
 import { InterpolationComponent } from './interpolation/interpolation.component'
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'home', pathMatch:'full'},
   {path:'home', component:HomeComponent},
   {path:'interpolation', component:InterpolationComponent},
   {path:'propb', component:PropertyBindingComponent},
-  {path:'eventb', component:EventBindingComponent}
+  {path:'eventb', component:EventBindingComponent},
+  {path:'two-way', component:TwoWayBindingComponent}
 ];
 
 @NgModule({
