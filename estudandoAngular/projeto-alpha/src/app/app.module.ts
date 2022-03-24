a4c63de25ffa56929730b91a23525ad34e04ab0d
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-24 19:29:29 -0300

    Criando o componente event-binding

--- estudandoAngular/projeto-alpha/src/app/app.module.ts
+++ estudandoAngular/projeto-alpha/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { HomeComponent } from './home/home.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InterpolationComponent,
     HomeComponent,
-    PropertyBindingComponent
+    PropertyBindingComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule,
