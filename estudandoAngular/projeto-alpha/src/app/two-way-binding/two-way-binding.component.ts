616afd5bdbb08d70e83852b3c006d668c628e580
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-24 20:15:54 -0300

    Editando nosso componente two way

--- estudandoAngular/projeto-alpha/src/app/two-way-binding/two-way-binding.component.ts
+++ estudandoAngular/projeto-alpha/src/app/two-way-binding/two-way-binding.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-two-way-binding',
   templateUrl: './two-way-binding.component.html',
   styleUrls: ['./two-way-binding.component.css']
 })
-export class TwoWayBindingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TwoWayBindingComponent {
+  //propriedade que será vinculada via two-way binding
+  nome!: string
 }
