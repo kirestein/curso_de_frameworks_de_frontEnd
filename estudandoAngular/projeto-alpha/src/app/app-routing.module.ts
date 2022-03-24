65edb1ba36cc377b45b1e57f240609acfdc7d7ed
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-24 20:00:04 -0300

    Adicionando uma nova rota

--- estudandoAngular/projeto-alpha/src/app/app-routing.module.ts
+++ estudandoAngular/projeto-alpha/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { InterpolationComponent } from './interpolation/interpolation.component'
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'home', pathMatch:'full'},
   {path:'home', component:HomeComponent},
   {path:'interpolation', component:InterpolationComponent},
-  {path:'propb', component:PropertyBindingComponent}
+  {path:'propb', component:PropertyBindingComponent},
+  {path:'eventb', component:EventBindingComponent}
 ];
 
 @NgModule({
