ca7f2a1ee99a87ae1c513f6d4301e1270e0d9423
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-24 22:35:24 -0300

    Editando o TS do output

--- estudandoAngular/projeto-alpha/src/app/output/output.component.ts
+++ estudandoAngular/projeto-alpha/src/app/output/output.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html',
   styleUrls: ['./output.component.css']
 })
-export class OutputComponent implements OnInit {
+export class OutputComponent {
+  //Vamos criar um anunciador do evento que enviará um dataset para o pai
+  @Output() anunciador = new EventEmitter<any>()
 
-  constructor() { }
+  //vamos criar um objeto literal para receber o conjunto de dados
+  objDados: any = {}
 
-  ngOnInit(): void {
+  //vamos criar um método para USANDO O ANUNCIADOR enviar os dados
+  enviarDados(): void {
+    this.anunciador.emit(this.objDados)
   }
-
 }
