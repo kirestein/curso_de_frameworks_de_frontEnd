af040726a96842e5e3366819d7d1af8ba1027985
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-24 22:06:27 -0300

    Editando o TS do pai

--- estudandoAngular/projeto-alpha/src/app/c-pai/c-pai.component.ts
+++ estudandoAngular/projeto-alpha/src/app/c-pai/c-pai.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-c-pai',
   templateUrl: './c-pai.component.html',
   styleUrls: ['./c-pai.component.css']
 })
-export class CPaiComponent implements OnInit {
+export class CPaiComponent {
+  title = 'Angular-Alpha'
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  //criar as propertys cujos valores serão enviados para o componente filho
+  texto: string = 'Este é o texto que será exibido na janela de alerta'
+  outroTexto: string = 'Texto enviado do componente-pai para o componente-filho'
 }
