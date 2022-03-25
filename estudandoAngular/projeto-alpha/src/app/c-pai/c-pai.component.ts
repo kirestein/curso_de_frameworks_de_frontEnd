b2400e88883b5f99e3581debda7f6a1073d00f5f
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-24 21:51:02 -0300

    Criando o componente pai

---
+++ estudandoAngular/projeto-alpha/src/app/c-pai/c-pai.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c-pai',
+  templateUrl: './c-pai.component.html',
+  styleUrls: ['./c-pai.component.css']
+})
+export class CPaiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
