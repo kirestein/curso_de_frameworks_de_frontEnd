3b826df33802b4ab1e829a4a29400e1b856433ec
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-24 22:58:08 -0300

    Editando o TS do pai

--- estudandoAngular/projeto-alpha/src/app/c-pai/c-pai.component.ts
+++ estudandoAngular/projeto-alpha/src/app/c-pai/c-pai.component.ts
@@ -11,4 +11,13 @@ export class CPaiComponent {
   //criar as propertys cujos valores serão enviados para o componente filho
   texto: string = 'Este é o texto que será exibido na janela de alerta'
   outroTexto: string = 'Texto enviado do componente-pai para o componente-filho'
+
+  //vamos criar um método para receber os dados e vincular na viw do pai
+  recebendoDados(dadosRecebidos: any) {
+    let receptora: string = `${dadosRecebidos.nome} obrigado por se cadastrar \n`
+
+    receptora = `${receptora} o email ${dadosRecebidos.email} foi cadastrado com sucesso!`
+
+    alert(receptora)
+  }
 }
