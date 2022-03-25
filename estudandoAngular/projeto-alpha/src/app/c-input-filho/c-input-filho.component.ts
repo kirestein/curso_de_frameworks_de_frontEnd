3f20a83af492a86a2a7a9439ed24974b4c1be96a
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-24 21:50:32 -0300

    Criando o componente filho

---
+++ estudandoAngular/projeto-alpha/src/app/c-input-filho/c-input-filho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c-input-filho',
+  templateUrl: './c-input-filho.component.html',
+  styleUrls: ['./c-input-filho.component.css']
+})
+export class CInputFilhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
