47bedd7c7bf2d54e761b44f77cbbe99f04b76b71
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-28 20:06:53 -0300

    Editando o arquivo TS do Pipe

--- estudandoAngular/projeto-alpha/src/app/pipe/pipe.component.ts
+++ estudandoAngular/projeto-alpha/src/app/pipe/pipe.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipe',
   templateUrl: './pipe.component.html',
   styleUrls: ['./pipe.component.css']
 })
-export class PipeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class PipeComponent {
+  //vamos implementar as props Pipe
+  numFloat: number = 678.94
+  dataHoje: Date = new Date()
+  numDecimal: number = 4893547.9563
+  objLiteral = {
+    name: 'Erik',
+    address: {
+      street: 'Rua Dos Bobos',
+      number: '0'
+    }
   }
-
+  mesesAno: Array<string> = ['Jan', 'Fev', 'Mar', 'Abril', 'Maio', 'Junho', 'Julho', 'Agos', 'Set', 'Nov', 'Dez']
+  numPercentual: number = 0.35
 }
