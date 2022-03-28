d2b76d011d1af11cc39d548b9f718f0a08b89f4c
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-28 19:21:04 -0300

    Editando o TS do ng-style

--- estudandoAngular/projeto-alpha/src/app/ng-style/ng-style.component.ts
+++ estudandoAngular/projeto-alpha/src/app/ng-style/ng-style.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-style',
   templateUrl: './ng-style.component.html',
   styleUrls: ['./ng-style.component.css']
 })
-export class NgStyleComponent implements OnInit {
+export class NgStyleComponent {
+  //vamos criar as propriedades para vincularmos via ngStyle
+  cores: string = '#380f6c'
+  tamanho: number = 129
 
-  constructor() { }
+  //vamos instanciar a nossa classe
+  objStyle: EstiloCss = new EstiloCss()
+}
 
-  ngOnInit(): void {
-  }
-
+//Criando a classe para vincular a diretiva ngStyle
+class EstiloCss {
+  'color': string = '#33c9ff'
+  'font-size': number = 80
+  'font-weight': string = 'bold'
 }
