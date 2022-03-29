a1e462ead6e2111101d97b2b01a68875cb0be655
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-28 22:22:20 -0300

    Utilizando Form Group e FormControl

--- estudandoAngular/projeto-alpha/src/app/formulario/formulario.component.ts
+++ estudandoAngular/projeto-alpha/src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -9,5 +10,22 @@ export class FormularioComponent {
   //vamos criar um método para receber os dados do formulário
   recebedorDados(dadosRecebidos: any) {
     alert(`O email recebido foi: ${dadosRecebidos.email}`)
+  }
+
+  //Criando as props para controlar o formulário
+  dataForm: any
+  email: any
+
+  //chamando o hook para priorizar a instância da classe
+  ngOnInit() {
+    this.dataForm = new FormGroup({
+      email: new FormControl('angular@mail.com'),
+      senha: new FormControl('@#$%')
+    })
+  }
+
+  //Vamos criar um método para exibir o resultado do "controle" de formulário pela camada lógica
+  exibirDados(umDado:any) {
+    this.email = umDado.email
   }
 }
