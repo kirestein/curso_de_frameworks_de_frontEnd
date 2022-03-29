d2a5c495b192a24cc857d5aa2cf44f7d1fe68cf6
eriksteindorfer <erik.steindorfer@@i9tec.com.br>
2022-03-28 21:55:10 -0300

    Editando nosso formulário

--- estudandoAngular/projeto-alpha/src/app/formulario/formulario.component.ts
+++ estudandoAngular/projeto-alpha/src/app/formulario/formulario.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FormularioComponent {
+  //vamos criar um método para receber os dados do formulário
+  recebedorDados(dadosRecebidos: any) {
+    alert(`O email recebido foi: ${dadosRecebidos.email}`)
   }
-
 }
